2a2a6eaead47273a25356b286be54a2b0a4d8d98
Zijian Zhang <doomspec@outlook.com>
2024-05-29 11:43:38 +0800

    docs: add docs

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,43 @@
+import {defineUserConfig} from "@vuepress/cli"
+import {defaultTheme} from '@vuepress/theme-default'
+import * as fs from 'fs';
+
+function get_md_files_names_in_path(path: string) {
+    const files = fs.readdirSync(path);
+    let file_paths = files.filter((file: string) => file.endsWith('.md'));
+    // Filter index.md
+    file_paths = file_paths.filter((file: string) => file !== 'index.md');
+    return file_paths;
+}
+
+export default defineUserConfig({
+    repo: 'EvoEvolver/MinimalLLM',
+    docsDir: 'docs',
+    head: [['link', { rel: 'icon', href: '/image/favicon.ico' }]],
+    // @ts-ignore
+    theme: defaultTheme({
+        logo: '/image/logo.png',
+        navbar: [
+            {
+                text: 'Home',
+                link: '/',
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/EvoEvolver/MinimalLLM',
+            }
+        ],
+        sidebar: {
+            "/": [{
+                text: 'MinimalLLM Docs',
+                children: [
+                    {
+                        text: 'Home',
+                        link: '/',
+                    },
+                ],
+            }],
+        },
+        sidebarDepth: 2,
+    })
+})
---
+++ docs/README.md
@@ -0,0 +1,22 @@
+## Website development setup
+
+Install pnpm
+```shell
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+```
+
+Install dependencies
+```shell
+pnpm i
+```
+
+Dev mode for the docs
+```shell
+pnpm docs:dev
+```
+
+## Website content development
+
+Edit sidebars
+
+- See `.vuepress/config.ts`.
