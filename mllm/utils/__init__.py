d5ef8a39e34fd0ed803d2acfbf698d904c1d0750
Zijian Zhang <doomspec@outlook.com>
2024-06-10 21:41:21 +0100

    fix: add parallel_map to utils

--- mllm/utils/__init__.py
+++ mllm/utils/__init__.py
@@ -4,3 +4,5 @@ import sys
 def debugger_is_active() -> bool:
     """Return if the debugger is currently active"""
     return hasattr(sys, 'gettrace') and sys.gettrace() is not None
+
+from .maps import parallel_map
