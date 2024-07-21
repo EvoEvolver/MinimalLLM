424c2b69e8147516eb6e9d7da05d38c413ee998d
Zijian Zhang <doomspec@outlook.com>
2024-07-21 16:24:38 -0400

    fix: support replicate/meta-llama

--- mllm/special_models.py
+++ mllm/special_models.py
@@ -32,15 +32,26 @@ def get_llava_response(messages, options, model_name):
 
 
 def get_llama_response(messages, options, model_name):
+    try:
+        import replicate
+    except ImportError:
+        raise ImportError("Please install the `replicate` package to use the llava model")
+    model_name = model_name.split("/")[1:]
+    model_name = "/".join(model_name)
     prompt, image = extract_prompt_and_image(messages)
-    new_messages = [
-        {
-            "role": "user",
-            "content": prompt
+    output = replicate.run(
+        model_name,
+        input={
+            "top_p": 1,
+            "prompt": prompt,
+            "history": [],
+            "max_tokens": 2048*2,
+            "temperature": options["temperature"],
         }
-    ]
-    res = completion(model_name, messages=new_messages, **options).choices[0].message.content
+    )
+    res = "".join(output)
     return res
+
 
 def extract_prompt_and_image(messages):
     prompt = []
@@ -68,6 +79,7 @@ special_models = {
     "replicate/yorickvp/llava-v1.6-34b:41ecfbfb261e6c1adf3ad896c9066ca98346996d7c4045c5bc944a79d430f174": get_llava_response
 }
 
+
 def get_special_model_handler(model_name):
     if not model_name.startswith("replicate/"):
         return None
@@ -75,8 +87,3 @@ def get_special_model_handler(model_name):
         return lambda messages, options: get_llava_response(messages, options, model_name)
     if "llama" in model_name:
         return lambda messages, options: get_llama_response(messages, options, model_name)
-
-
-
-
-
