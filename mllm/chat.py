9d8eb3160d5d61c907a4b2c5cda00ecc576bb965
Zijian Zhang <doomspec@outlook.com>
2024-09-02 17:10:18 -0400

    feat: add cache info in additional_res

--- mllm/chat.py
+++ mllm/chat.py
@@ -246,15 +246,16 @@ class Chat:
         messages = self.get_messages_to_api()
         mock_response = None
         cache = None
+        additional_res = {}
         if use_cache:
             cache = caching.cache_kv.read_cache(messages, "chat_"+model)
             if cache is not None and cache.is_valid():
                 mock_response = cache.value
+                additional_res["cache_hit"] = True
                 # Avoid unnecessary cache rewriting
                 use_cache = False
 
         options = options or {}
-        additional_res = {}
         special_handler = get_special_model_handler(model)
         if special_handler is None:
             response = completion(model, messages=messages, mock_response=mock_response, **options)
